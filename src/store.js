c69a75004b51322a04fc6cea5763182014c83b1f
神田真也 <kandashinya@MacBook-Pro.local>
2020-09-06 11:49:48 +0900

    master

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state:{
+        count:10,
+        message:"hell0"
+    },
+    getters:{
+        getCount:function(){
+            return this.state.count
+        },
+        getMessage:function(){
+            return this.state.message
+        }
+    }
+})
